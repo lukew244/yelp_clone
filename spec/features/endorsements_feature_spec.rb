0590d99ebc6ad7b8893a13e86cd3ede3c549f6b0
lukew244 <luke.webster86@gmail.com>
2016-08-26 13:21:25 +0100

    Adds database cleaner

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -12,7 +12,7 @@ feature 'endorsing reviews' do
     click_button 'Leave Review'
   end
 
-  scenario 'a user can endorse a review, which updates the review endorsement count' do
+  scenario 'a user can endorse a review, which updates the review endorsement count', js: true do
     visit '/restaurants'
     click_link 'Endorse Review'
     expect(page).to have_content('1 endorsement')
